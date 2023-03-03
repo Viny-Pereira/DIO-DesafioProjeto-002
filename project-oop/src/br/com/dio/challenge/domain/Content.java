aba0cf866fdb9c9bd174af6d8f7007eb756079b0
Viny-Pereira <vinypereira75@gmail.com>
2023-03-03 19:34:08 -0300

    create content

---
+++ project-oop/src/br/com/dio/challenge/domain/Content.java
@@ -0,0 +1,30 @@
+package br.com.dio.challenge.domain;
+
+public abstract class Content {
+    protected static final double XP_DEFAULT = 10d;
+
+    private String title;
+    private String description;
+
+    public abstract double calculate();
+
+    public static double getXpDefault() {
+        return XP_DEFAULT;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
